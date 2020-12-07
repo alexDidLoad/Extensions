e8cbd08b152fa08f217b6384632a33a2187b9add
Alexander Ha <Ha.alex.h@gmail.com>
2020-12-06 18:41:26 -0800

    Add anchor extensions for UIView

--- bounceOut.playground/Contents.swift
+++ bounceOut.playground/Contents.swift
@@ -6,7 +6,7 @@ import PlaygroundSupport
 extension UIView {
     
     func bounceOut(duration: TimeInterval) {
-       
+        
         UIView.animate(withDuration: duration, animations: { [unowned self] in
             self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
         })
@@ -15,16 +15,59 @@ extension UIView {
 /* bounceOut(duration:) Test */
 let mainView = UIView(frame: CGRect(x: 0, y: 0, width: 360, height: 360))
 mainView.translatesAutoresizingMaskIntoConstraints = false
-mainView.backgroundColor = UIColor.red
+mainView.backgroundColor = UIColor.white
 
 PlaygroundPage.current.liveView = mainView
 
-let subview = UIView(frame: CGRect(x: 0, y: 0, width: 240, height: 240))
-subview.translatesAutoresizingMaskIntoConstraints = false
-subview.backgroundColor = UIColor.blue
-subview.center.y = mainView.center.y
-subview.center.x = mainView.center.x
-mainView.addSubview(subview)
+//let subview = UIView(frame: CGRect(x: 0, y: 0, width: 240, height: 240))
+//subview.translatesAutoresizingMaskIntoConstraints = false
+//subview.backgroundColor = UIColor.blue
+//subview.center.y = mainView.center.y
+//subview.center.x = mainView.center.x
+//mainView.addSubview(subview)
+//
+//subview.bounceOut(duration: 2)
 
-subview.bounceOut(duration: 2)
+class myButton: UIButton {
+    
+    var imageName: String?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+        
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        
+        let shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+        let shadowOffSet = CGSize(width: 0, height: 10.0)
+        let shadowOpacity = Float(1.0)
+        let shadowRadius = CGFloat(10.0)
+        
+        self.layer.masksToBounds = false
+        self.layer.borderWidth = 3
+        self.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).cgColor
+        self.layer.shadowColor = shadowColor
+        self.layer.shadowOffset = shadowOffSet
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowRadius = shadowRadius
+        self.layer.cornerRadius = 0.5 * self.frame.size.width
+        self.setImage(UIImage(named: "\(String(describing: imageName))"), for: .normal)
+        self.backgroundColor = UIColor.white
+        
+    }
+}
+
+let testButton = myButton(frame: CGRect(x: 20, y: 20, width: 150, height: 150))
+
+mainView.addSubview(testButton)
+
 
